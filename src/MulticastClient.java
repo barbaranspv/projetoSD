c50a8868ec1e52e95c986d8e67f50e3a0d014d4d
barbaravieira99 <44678064+barbaravieira99@users.noreply.github.com>
2019-10-20 16:24:50 +0100

    Add files via upload

---
+++ src/MulticastClient.java
@@ -0,0 +1,101 @@
+import java.io.Serializable;
+import java.net.MulticastSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * The MulticastClient class joins a multicast group and loops receiving
+ * messages from that group. The client also runs a MulticastUser thread that
+ * loops reading a string from the keyboard and multicasting it to the group.
+ * <p>
+ * The example IPv4 address chosen may require you to use a VM option to
+ * prefer IPv4 (if your operating system uses IPv6 sockets by default).
+ * <p>
+ * Usage: java -Djava.net.preferIPv4Stack=true MulticastClient
+ *
+ * @author Raul Barbosa
+ * @version 1.0
+ */
+public class MulticastClient extends Thread {
+    private String MULTICAST_ADDRESS = "224.0.224.0";
+    private int PORT = 4321;
+
+    public static void main(String[] args) {
+        MulticastClient client = new MulticastClient();
+        client.start();
+        MulticastUser user = new MulticastUser();
+        user.start();
+    }
+
+    public void run() {
+        MulticastSocket socket = null;
+        try {
+            socket = new MulticastSocket(PORT);  // create socket and bind it
+            InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
+            socket.joinGroup(group);
+            while (true) {
+                byte[] buffer = new byte[256];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+
+                System.out.println("Received packet from " + packet.getAddress().getHostAddress() + ":" + packet.getPort() + " with message:");
+                String message = new String(packet.getData(), 0, packet.getLength());
+                String[] result = message.split(" ; ");
+                String[] type = result[0].split(" ! ");
+                //System.out.println(type[1]);
+                if (type[1].equals("login") ){
+                    String username=result[1].split(" ! ")[1];
+                    //System.out.println(username);
+                    String password=result[2].split(" ! ")[1];
+                    System.out.println(username + " "+ password);
+                }
+                System.out.println(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            socket.close();
+        }
+    }
+}
+
+class MulticastUser extends Thread {
+    private String MULTICAST_ADDRESS = "224.0.224.0";
+    private int PORT = 4321;
+
+    public MulticastUser() {
+        super("User " + (long) (Math.random() * 1000));
+    }
+
+    public void run() {
+        MulticastSocket socket = null;
+        System.out.println(this.getName() + " ready...");
+        try {
+            socket = new MulticastSocket();  // create socket without binding it (only for sending)
+            Scanner keyboardScanner = new Scanner(System.in);
+            while (true) {
+                String readKeyboard = keyboardScanner.nextLine();
+                byte[] buffer = readKeyboard.getBytes();
+
+                InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, PORT);
+                socket.send(packet);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            socket.close();
+        }
+    }
+}
+class Utilizador implements Serializable {
+    String username;
+    String password;
+
+    Utilizador(String username,String password){
+        this.username=username;
+        this.password=password;
+    }
+}
